9aacbe16418faa8a8ddb864722a58c5ed32cde34
Stanislav Sheinoha <stanyslav.sheinoha@gmail.com>
2023-02-14 22:04:20 +0200

    Completes the stylization of the secondtask

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,16 +1,28 @@
+import StatisticsModule from '../components/Statistics.module.css';
+// export function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
 export const Statistics = data => {
   const { title, stats } = data;
-
+  // const itemColor = {
+  //   background: getRandomHexColor(),
+  // };
   return (
     <>
-      <section className="statistics">
-        {title.length > 0 && <h2>{title}</h2>}
+      <section className={StatisticsModule.statistics}>
+        {title && <h2 className={StatisticsModule.title}>{title}</h2>}
 
-        <ul className="stat-list">
+        <ul className={StatisticsModule.statList}>
           {stats.map(stat => (
-            <li key={stat.id} className="item">
-              <span className="label">{stat.label}</span>
-              <span className="percentage">{stat.percentage}%</span>
+            <li
+              key={stat.id}
+              // style={itemColor}
+              className={StatisticsModule.item}
+            >
+              <span className={StatisticsModule.label}>{stat.label}</span>
+              <span className={StatisticsModule.percentage}>
+                {stat.percentage}%
+              </span>
             </li>
           ))}
         </ul>
