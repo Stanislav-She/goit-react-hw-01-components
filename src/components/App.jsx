d156f60d730db9311285cf35cc5a88869121809a
Stanislav Sheinoha <stanyslav.sheinoha@gmail.com>
2023-02-14 16:07:11 +0200

    Finalizes third task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import { Profile } from './Profile';
 import { Statistics } from './Statistics';
+import { FriendList } from './FriendList';
 import user from '../data/user.json';
 import data from '../data/data.json';
+import friends from '../data/friends.json';
 
 export const App = () => {
   return (
@@ -13,8 +15,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
     </>
   );
 };
---
+++ src/components/FriendList.jsx
@@ -0,0 +1,22 @@
+export const FriendList = data => {
+  const { friends } = data;
+
+  return (
+    <>
+      <ul className="friend-list">
+        {friends.map(friend => (
+          <li key={friend.id} className="item">
+            <span className="status">{friend.isOnline}</span>
+            <img
+              className="avatar"
+              src={friend.avatar}
+              alt="User avatar"
+              width="48"
+            />
+            <p className="name">{friend.name}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
